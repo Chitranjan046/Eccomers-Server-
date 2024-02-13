44f82c234e5beccf393bd880f97c372809458a6f
Chitranjan046 <chitranjankumarpatel8@gmail.com>
2024-02-13 17:31:39 +0530

    User Controllers don

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// importing routes
+const user_js_1 = __importDefault(require("./routes/user.js"));
 const port = 4000;
 const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    req.send("API Working with/ api/v1");
+});
+// using Routes
+app.use("./api/v1/user", user_js_1.default);
 app.listen(port, () => {
     console.log(`Server is running on:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 import express from "express";
 
+// importing routes
+import userRoute from "./routes/user.js";
+
 const port = 4000;
+connectDB();
 
 const app= express();
+
+app.use(express.json());
+
+
+
+app.get("/", (req, res)=>{
+    req.send("API Working with/ api/v1")
+});
+// using Routes
+
+app.use("./api/v1/user", userRoute );
+
+
 
 app.listen(port, () =>{
     console.log(`Server is running on:${port}`);
