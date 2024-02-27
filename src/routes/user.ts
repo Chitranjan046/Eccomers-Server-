b0155d596cb34ea85b0faf2f00b3666de9f75731
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-02-27 16:26:05 +0530

    Delete User functionality  don

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,7 @@ import {
   getAllUsers,
   newUser,
   getUser,
+  deleteUser,
 } from "../controllers/user.js";
 
 const app = express.Router();
@@ -14,6 +15,6 @@ app.post("/new", newUser);
 app.get("/all",  getAllUsers);
 
 // Route - /api/v1/user/dynamicID
-app.get("/:id", getUser);
+app.route("/:id").get(getUser).delete( deleteUser);
 
 export default app;
